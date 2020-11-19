02b337f3a51a4617e5511828e4d0a5cef4e81b49
hahajing <444511958@qq.com>
2020-11-19 18:13:44 +0800

    first commit

---
+++ CoreLib/src/main/java/com/zy/corelib/TestUtils.java
@@ -0,0 +1,13 @@
+package com.zy.corelib;
+
+import android.util.Log;
+
+/**
+ * @author:zhangyue
+ * @date:2020/11/19
+ */
+public class TestUtils {
+    public static void testMethod(){
+        Log.d("123","testMethod is called...");
+    }
+}
